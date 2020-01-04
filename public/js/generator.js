9559358aec2f508abbb5984166d8e318acea0bb8
Daniel Martin <dmartin@webimpacto.es>
2020-01-04 13:05:02 +0100

    changes over UI and remove node-modules

--- public/js/generator.js
+++ public/js/generator.js
@@ -32,34 +32,6 @@ controls.enableDamping = true;
 controls.dampingFactor = 1;
 controls.zoomSpeed = 5;
 
-
-function resize() {
-    var ratio = 16 / 9,
-    preHeight = documentContainer.clientWidth / ratio;
-
-    
-    if (preHeight <= documentContainer.clientHeight) {
-        renderer.setSize(documentContainer.clientWidth, preHeight);
-        ctx.canvas.width = documentContainer.clientWidth;
-        ctx.canvas.height = preHeight;
-    } else {
-        var newWidth = Math.floor(documentContainer.clientWidth - (preHeight - documentContainer.clientHeight) * ratio);
-        newWidth -= newWidth % 2 !== 0 ? 1 : 0;
-        renderer.setSize(newWidth, newWidth / ratio);
-        ctx.canvas.width = newWidth;
-        ctx.canvas.height = newWidth / ratio;
-    }
-
-    renderer.domElement.style.width = '';
-    renderer.domElement.style.height = '';
-    renderer.domElement.style.left = ctx.canvas.style.left = (documentContainer.clientWidth - renderer.domElement.width) / 2 + 'px';
-    renderer.domElement.style.top = ctx.canvas.style.top = (documentContainer.clientHeight - renderer.domElement.height) / 2 + 'px';
-}
-
-window.addEventListener('resize', resize);
-
-resize();
-
 //Objects
 createStar();
 
